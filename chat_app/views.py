6f821673a8780c65da4a9d08b0913b38deaf1ea9
camiloleal <camiloandreslealarias@gmail.com>
2014-05-16 02:45:57 -0500

    Templates basicos

--- chat_app/views.py
+++ chat_app/views.py
@@ -2,19 +2,19 @@ from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 
 def logIn(request):
-	return render_to_response('logIn.html')
+	return render_to_response('chat_app/logIn.html')
 
 def signUp(request):
-	return render_to_response('signUp.html')
+	return render_to_response('chat_app/signUp.html')
 
 def home(request):
-	return render_to_response('home.html')
+	return render_to_response('chat_app/home.html')
 
 def groups(request):
-	return render_to_response('groups.html')
+	return render_to_response('chat_app/groups.html')
 
 def generalChat(request):
-	return render_to_response('generalChat.html')
+	return render_to_response('chat_app/generalChat.html')
 
 def groupChat(request):
-	return render_to_response('groupChat.html')
+	return render_to_response('chat_app/groupChat.html')
